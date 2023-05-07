87cc7451d5cc229d60ccef18bec506f4c6793624
Lalitha0525 <124656922+Lalitha0525@users.noreply.github.com>
2023-05-07 20:53:26 +0530

    Create if statements.py

---
+++ if statements.py
@@ -0,0 +1,8 @@
+temperature = 30
+
+if temperature < 40 and temperature > 20:
+    print("its a very hot day")
+    print("Drink more water")
+elif tempetarute < 20 and temperature < 10:
+    print("its a normal day")
+print("done")
